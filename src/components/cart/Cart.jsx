a38e6b29eb177408f31aad92157aa5a1c38f6aa4
DevShikzia <devsanchezmatias@gmail.com>
2022-10-01 03:30:22 -0300

    orden de compra terminado

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,11 +1,56 @@
 import { useContext } from "react";
 import {CartContext} from "../cartContext/CartContext";
 import { Link } from 'react-router-dom'
+import { serverTimestamp, collection, doc, setDoc, updateDoc, increment } from "firebase/firestore";
+import {db} from '../../utils/firebaseConfig'
+import Swal from "sweetalert2";
 
 const Cart = () => {
 
 
 const {cartList,clear,removeItem,itemPriceTotal,totalPrice} = useContext(CartContext)
+
+const createOrder = async() => {
+
+  let itemsForDB = cartList.map(item => ({
+    id: item.id,
+    title: item.title,
+    price: item.price,
+    quantity: item.quantity,
+ }))
+
+  let order = {
+    buyer : {
+      name: 'Killua Zoldyck',
+      email: 'hisokaWTF@gmail.com',
+      phone: '123456789'
+    },
+    date: serverTimestamp(),
+    items: itemsForDB,
+    total: totalPrice(),
+  }
+  // Add a new document with a generated id
+  const newOrder = doc(collection(db, "orders"));
+  
+  // later...
+  await setDoc(newOrder, order)
+  
+  Swal.fire(`Compra existosa, su número de orden es: ${newOrder.id}`)
+  
+  clear()
+  
+  itemsForDB.map( async(item )=> {
+
+    
+    const itemRef = doc(db, "products", item.id);
+    
+    // Set the "capital" field of the city 'DC'
+    await updateDoc(itemRef, {
+      stock: increment(-item.quantity)
+    })
+    
+  })
+}
 
   return (
     <>
@@ -43,6 +88,7 @@ const {cartList,clear,removeItem,itemPriceTotal,totalPrice} = useContext(CartCon
             
             <div className=" d-flex justify-content-between align-items-start">
                 <button onClick={clear}>Delete all</button>
+                <button onClick={createOrder}>Terminar compra</button>
                 <span className="badge bg-primary rounded-pill">TOTAL: ${totalPrice()}</span>
           </div>
           }
